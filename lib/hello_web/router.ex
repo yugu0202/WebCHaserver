8f412c463eaab824e43fb424bb8e54aa86b7c255
yugu0202 <applerin1119@gmail.com>
2023-10-03 02:31:32 +0000

    CD test

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -18,6 +18,7 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+		get "/hello", PageController, :home
   end
 
   # Other scopes may use custom stacks.
