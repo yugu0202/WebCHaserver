1aeeecb00638de86eae686ca2785043a3ad2d9ae
yugu-0202 <applerin1119@gmail.com>
2023-11-08 12:19:28 +0900

    fix schema

--- lib/webchaserver/matchs/match.ex
+++ lib/webchaserver/matchs/match.ex
@@ -5,7 +5,6 @@ defmodule Webchaserver.Matchs.Match do
   schema "matchs" do
     field(:cool_pos, :string)
     field(:hot_pos, :string)
-    field(:match_id, :integer)
     field(:map, :string)
 
     timestamps()
@@ -14,7 +13,7 @@ defmodule Webchaserver.Matchs.Match do
   @doc false
   def changeset(match, attrs) do
     match
-    |> cast(attrs, [:match_id, :map, :cool_pos, :hot_pos])
-    |> validate_required([:match_id, :map, :cool_pos, :hot_pos])
+    |> cast(attrs, [:map, :cool_pos, :hot_pos])
+    |> validate_required([:map, :cool_pos, :hot_pos])
   end
 end
--- priv/repo/migrations/20231101162328_create_matchs.exs
+++ priv/repo/migrations/20231101162328_create_matchs.exs
@@ -3,7 +3,6 @@ defmodule Webchaserver.Repo.Migrations.CreateMatchs do
 
   def change do
     create table(:matchs) do
-      add(:match_id, :integer)
       add(:map, :string)
       add(:cool_pos, :string)
       add(:hot_pos, :string)
